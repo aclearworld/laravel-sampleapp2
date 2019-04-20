4854b192e5c7e20757f7ecbb67b190a9db286d41
yamamichi <yamamichi1995@outlook.jp>
2019-04-20 12:43:39 +0900

    ファクトリー　記載

--- app/Report.php
+++ app/Report.php
@@ -4,6 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Report
+ *
+ * @property-read \App\Customer $reports
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report query()
+ * @mixin \Eloquent
+ */
 class Report extends Model
 {
     /**
