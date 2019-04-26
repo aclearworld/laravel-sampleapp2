0f87bd0568b6f2c3f7a2f28da08779ad82f9bc5e
yamamichi <yamamichi1995@outlook.jp>
2019-04-26 22:25:10 +0900

    tdd

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('customers',function (){});
-Route::post('customers',function (){});
-Route::get('customers/{customer_id}',function (){});
-Route::put('customers/{customer_id}',function (){});
-Route::delete('customers/{customer_id}',function (){});
-Route::get('reports',function (){});
-Route::post('reports',function (){});
-Route::get('reports/{report_id}',function (){});
-Route::put('reports/{report_id}',function (){});
-Route::delete('reports/{report_id}',function (){});
+Route::get('customers', function () {
+    return response()->json(\App\Customer::query()->select(['id', 'name'])->get());
+});
+
+Route::post('customers', function (\Illuminate\Http\Request $request) {
+    if (!$request->json('name')){
+        return response()->json([], \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    $customer = new \App\Customer();
+    $customer->name = $request->json('name');
+    $customer->save();
+});
+
+Route::get('customers/{customer_id}', function () {
+});
+Route::put('customers/{customer_id}', function () {
+});
+Route::delete('customers/{customer_id}', function () {
+});
+Route::get('reports', function () {
+});
+Route::post('reports', function () {
+});
+Route::get('reports/{report_id}', function () {
+});
+Route::put('reports/{report_id}', function () {
+});
+Route::delete('reports/{report_id}', function () {
+});
 
