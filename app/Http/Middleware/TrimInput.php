0d0fc6e5ec1b56b1be14ccae9624ed94fb0dbcc1
yamamichi <yamamichi1995@outlook.jp>
2019-05-06 18:57:43 +0900

    サービスプロバイダーに追記

--- app/Http/Middleware/TrimInput.php
+++ app/Http/Middleware/TrimInput.php
@@ -10,13 +10,13 @@ class TrimInput
     /**
      * リクエストに含まれる、前後の空白文字(全角含む)、を削除
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $utilityService =  new UtilityService(); //$this->app->make('App\Services\UtilityService');
+        $utilityService = app()->make('App\Services\UtilityService');
 
         $inputs = $request->all();
         $trimmed = [];
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\CustomerService;
+use App\Services\ReportService;
+use App\Services\UtilityService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(CustomerService::class);
+        $this->app->singleton(ReportService::class);
+        $this->app->singleton(UtilityService::class);
     }
 }
